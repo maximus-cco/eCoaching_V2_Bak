9b4f721aaf71122897eb8ca60643f52ddc80315a
211herbga <211herbga@vngt.tfs.local>
2015-05-28 14:35:27 +0000

    [VM:Susmitha.palacherla@2/20/2015 4:46:10 PM] Internal version 10. Updated for SCR 14375.
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C13962

--- Code/CCO_eCoaching_Functions.sql
+++ Code/CCO_eCoaching_Functions.sql
@@ -1,8 +1,12 @@
 /*
-File: eCoaching_Functions.sql (09)
+File: eCoaching_Functions.sql (10)
 Last Modified By: Susmitha Palacherla
-Date: 01/09/2015
+Date: 02/20/2015
 
+
+Version 10, 02/20/2015
+1. Modified fn #3 to handle multiple open ended employee id to lan id mappings per SCR 14375.
+    [EC].[fn_intGetSiteIDFromLanID]
 
 Version 09, 01/09/2015
 1. Added 1 function #24 and modified 1 fn # 22related to ETS compliance feed SCR 14031.
@@ -186,7 +190,7 @@ IF EXISTS (
    DROP FUNCTION [EC].[fn_intGetSiteIDFromLanID]
 GO
 
-/****** Object:  UserDefinedFunction [EC].[fn_intGetSiteIDFromLanID]    Script Date: 03/13/2014 15:26:40 ******/
+
 SET ANSI_NULLS ON
 GO
 
@@ -196,9 +200,12 @@ GO
 --	Author:		Susmitha Palacherla
 --	Create Date: 03/05/2014
 --	Description:	 
---  *  Given an LAN ID and a date, return the  Employee ID of the
---  person who had that LAN ID on that date.
--- The Employee ID is then used to look up the site and site ID of the user.
+ --Given an LAN ID and a date, returns the  Employee ID of the
+ --person who had that LAN ID on that date.
+ --The Employee ID is then used to look up the site and site ID of the user.
+ -- Last Modified By: Susmitha Palacherla
+ -- Last modified Date: 02/20/2015
+ -- Modified per SCR 14375 to handle multiple open ended employee ids mapped to a lanid
 --	=============================================
 CREATE FUNCTION [EC].[fn_intGetSiteIDFromLanID] 
 (
@@ -212,22 +219,101 @@ BEGIN
 	 DECLARE @nvcEmpID NVARCHAR(10),
 	         @intDate INT,
 	         @strSiteName NVARCHAR(30),
-	         @intSiteID INT
+	         @intSiteID INT,
+	         @intlanempid Int,
+	         @intehempid Int
 	  
 	
 	SET @intDate = EC.fn_intDatetime_to_YYYYMMDD (@dtmDate)
 	  
-	SET @nvcEmpID =
-	(
-	SELECT EmpID
+	-- Get count of Employee IDs for given lan ID in the Employe ID To lan ID Table
+	
+		SET @intlanempid = (
+	SELECT COUNT(EmpID)
 	FROM EC.EmployeeID_To_LanID
 	WHERE 
 	LanID = @nvclanID AND
 	@intDate BETWEEN StartDate AND EndDate
 	)
 	
-  IF  @nvcEmpID is NULL
-  SET @nvcEmpID = '999999'
+	-- IF at least one Employee ID is found
+	
+IF  @intlanempid > 0
+
+-- If exactly one Employee is Found return that Employee ID
+BEGIN
+
+IF  @intlanempid = 1
+	BEGIN
+	    SET @nvcEmpID = (
+	   SELECT DISTINCT EmpID
+	   FROM EC.EmployeeID_To_LanID
+	    WHERE 
+	    LanID = @nvclanID AND
+	    @intDate BETWEEN StartDate AND EndDate)
+	END
+	  
+ELSE 	
+
+-- If more than one Employee iD is found, return the one with the latest start date
+  
+	BEGIN
+	  	 SET @nvcEmpID = (
+		  SELECT DISTINCT LATEST.EmpID FROM
+		 (SELECT LAN.* FROM EC.EmployeeID_To_LanID LAN
+		  JOIN (SELECT LanID, MAX(StartDate)StartDate FROM [EC].[EmployeeID_To_LanID] GROUP BY LanID)MLAN
+		  ON LAN.LanID = MLAN.lanID 
+		  AND LAN.StartDate = MLAN.StartDate) AS LATEST
+		  WHERE LATEST.LanID =  @nvclanID AND 
+		  @intDate BETWEEN LATEST.StartDate AND LATEST.EndDate
+		  )
+     END
+	END     
+ELSE
+
+-- If No Employee Ids are found in the Employee ID To lan ID Table
+-- Get count of Employee IDs for given Lan ID in the Employe Hierarchy Table
+
+ BEGIN
+	SET @intehempid = (
+	SELECT COUNT(Emp_ID)
+	FROM EC.Employee_Hierarchy
+	WHERE Emp_LanID = @nvclanID AND
+	@intDate BETWEEN Start_Date AND End_Date
+	)
+-- If exactly one Employee ID is found return it
+	
+IF  @intehempid = 1	
+
+	 BEGIN
+	      SET @nvcEmpID = 
+		  (SELECT DISTINCT Emp_ID
+		  FROM EC.Employee_Hierarchy
+		  WHERE Emp_LanID = @nvclanID
+		  AND @intDate BETWEEN Start_Date AND End_Date
+		  )
+     END
+ -- If more than one Employee ID is found, return the one with the latest start date    
+ 
+  ELSE   
+       BEGIN
+	  	 SET @nvcEmpID = (
+		  SELECT DISTINCT LATEST.Emp_ID FROM
+		 (SELECT EH.* FROM EC.Employee_Hierarchy EH
+		  JOIN (SELECT Emp_LanID, MAX(Start_Date)Start_Date FROM EC.Employee_Hierarchy GROUP BY Emp_LanID)MEH
+		  ON EH.Emp_LanID = MEH.Emp_LanID 
+		  AND EH.Start_Date = MEH.Start_Date) AS LATEST
+		  WHERE LATEST.Emp_LanID =  @nvclanID AND 
+		  @intDate BETWEEN LATEST.Start_Date AND LATEST.End_Date
+		  )
+     END
+ END	
+ 
+ -- If no Employee ID found for given Lan ID in the Employee ID to Lan ID table
+ -- Then return '999999'
+   
+IF @nvcEmpID IS NULL 
+SET @nvcEmpID = '999999'
 	    
   SELECT  @strSiteName = [Emp_Site]
   FROM [EC].[Employee_Hierarchy]
@@ -241,13 +327,18 @@ BEGIN
   FROM [EC].[DIM_Site]
   WHERE [City] = @strSiteName
 
-  
-  
+   
   RETURN  @intSiteID
 
 	
 END --[EC].[fn_intGetSiteIDFromLanID] 
+
+
+
 GO
+
+
+
 
 
 *****************************************************************************
