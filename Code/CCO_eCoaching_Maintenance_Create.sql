0ff53f4946797bb0c8b1f7061f201329b8f07d02
211herbga <211herbga@vngt.tfs.local>
2015-05-28 14:37:40 +0000

    [VM:Susmitha.palacherla@4/30/2015 1:12:58 PM] Internal Version 07. Updated for Inactivations feed scr 14634.
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C14057

--- Code/CCO_eCoaching_Maintenance_Create.sql
+++ Code/CCO_eCoaching_Maintenance_Create.sql
@@ -1,31 +1,36 @@
 /*
-eCoaching_Maintenance_Create(06).sql
-Last Modified Date: 11/21/2014
+eCoaching_Maintenance_Create(07).sql
+Last Modified Date: 04/25/2015
 Last Modified By: Susmitha Palacherla
 
+Version 07: 
+1. Changes for SCR 14634 Inactivations from Feed.
+   Added Create for new table [EC].[Inactivations_Stage]
+   Added New procedure  #4 [EC].[sp_Inactivations_From_Feed] 
+   Marked Table #1 and SP #1 as obsolete.
 
 Version 06: 
-1. Updated SP [EC].[sp_SelectCoaching4Contact] to add source IDs 222,223,224
+1. Updated SP #2 [EC].[sp_SelectCoaching4Contact] to add source IDs 222,223,224
     for new Quality sources for SCR 13826.
 
 Version 05: 
-1. Updated SP [EC].[sp_SelectCoaching4Contact] to add source ID 221
+1. Updated SP #2 [EC].[sp_SelectCoaching4Contact] to add source ID 221
     for new ETS feed per SCR 13659.
 
 Version 04: 
-1. Updated procedures impacted by the Phase II Modular design.
+1. Updated procedure impacted by the Phase II Modular design.
 
 
 Version 03: 
-1. Updated [EC].[sp_Update_Migrated_User_Logs] per SCR 12982 
- to remove updates to Employee ID To Lan ID table until correct logic can be identified.
+1. Updated SP # 1[EC].[sp_Update_Migrated_User_Logs] per SCR 12982 
+    to remove updates to Employee ID To Lan ID table until correct logic can be identified.
 2. Removed historical dashboard related code which is being maintained in code doc
-CCO_eCoaching_Historical_Dashboard_Create.sql.
+    CCO_eCoaching_Historical_Dashboard_Create.sql.
 
 
 Version 02: 
 1. Updated PROCEDURE [EC].[sp_UpdateFeedMailSent]
- to set @sentValue to 1 per SCR 12957.
+    to set @sentValue to 1 per SCR 12957.
 
 
 Version 01: 
@@ -34,22 +39,23 @@ Version 01:
 Summary
 
 Tables
-1. Create Table  [EC].[Migrated_Employees]
+1. Create Table  [EC].[Migrated_Employees] -- Obsolete
+2. Create Table [EC].[Inactivations_Stage]
 
 
 
 Procedures
-1. Create SP [EC].[sp_Update_Migrated_User_Logs] 
+1. Create SP [EC].[sp_Update_Migrated_User_Logs] -- Obsolete
 2. Create SP [EC].[sp_SelectCoaching4Contact] 
 3. Create SP [EC].[sp_UpdateFeedMailSent] 
-
+4. Create SP [EC].[sp_Inactivations_From_Feed] 
 
 */
 
 
  --Details
 
-**************************************************************
+/**************************************************************
 --Tables
 **************************************************************
 
@@ -72,13 +78,28 @@ CREATE TABLE [EC].[Migrated_Employees](
 GO
 
 
+--2. Create Table [EC].[Inactivations_Stage]
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [EC].[Inactivations_Stage](
+	[FormName] [nvarchar](50) NULL,
+	[Message] [nvarchar](100) NULL,
+	[ProcessDate] [datetime] NULL
+) ON [PRIMARY]
+
+GO
 
 
-**************************************************************
+--**************************************************************/
 
 --Procedures
 
-**************************************************************
+--**************************************************************
 
 --1. Create SP  [EC].[sp_Update_Migrated_User_Logs] 
 
@@ -128,7 +149,7 @@ GO
 
 
 
-******************************************************************
+--******************************************************************
 
 -2. Create SP  [EC].[sp_SelectCoaching4Contact] 
 
@@ -226,7 +247,7 @@ GO
 
 
 
-***************************************************************************************************************
+--***************************************************************************************************************
 
 
 -3. Create SP  [EC].[sp_UpdateFeedMailSent] 
@@ -274,5 +295,178 @@ END --sp_UpdateFeedMailSent
 GO
 
 
-***************************************************************************************************************
+--***************************************************************************************************************
 
+-4. Create SP [EC].[sp_Inactivations_From_Feed] 
+
+IF EXISTS (
+  SELECT * 
+    FROM INFORMATION_SCHEMA.ROUTINES 
+   WHERE SPECIFIC_SCHEMA = N'EC'
+     AND SPECIFIC_NAME = N'sp_Inactivations_From_Feed' 
+)
+   DROP PROCEDURE [EC].[sp_Inactivations_From_Feed]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+-- =============================================
+-- Author:		   Susmitha Palacherla
+-- Create Date:    04/22/2015
+-- Description:	Inactivate Coaching and Warning logs from feed files.
+-- Initial revision per SCR 14634.
+-- =============================================
+CREATE PROCEDURE [EC].[sp_Inactivations_From_Feed] 
+@strLogType nvarchar(20)
+AS
+BEGIN
+
+
+
+ IF @strLogType = 'Coaching'
+ -- Coaching logs 
+ -- Set Message for Invalid Form Names
+
+BEGIN
+UPDATE [EC].[Inactivations_Stage]
+SET [Message] = 'Form Name does not Exist.'
+,[ProcessDate] = GETDATE()
+WHERE [FormName] NOT IN 
+(SELECT DISTINCT FormName FROM [EC].[Coaching_Log] C WITH (NOLOCK))
+OPTION (MAXDOP 1)
+END
+
+WAITFOR DELAY '00:00:00.05' -- Wait for 5 ms
+
+ -- Set Message for Invalid Status
+
+BEGIN
+UPDATE [EC].[Inactivations_Stage]
+SET [Message] = 'Invalid Status for Inactivation.'
+,[ProcessDate] = GETDATE()
+FROM [EC].[Inactivations_Stage] I JOIN [EC].[Coaching_Log] C 
+ON I.[FormName]= C.[FormName]
+WHERE C.[StatusID] IN (1,2)
+OPTION (MAXDOP 1)
+END
+
+
+WAITFOR DELAY '00:00:00.05' -- Wait for 5 ms
+
+
+-- Inactivate Coaching logs 
+
+BEGIN
+UPDATE [EC].[Coaching_Log]
+SET [StatusID] = 2
+FROM [EC].[Inactivations_Stage] I JOIN [EC].[Coaching_Log] C 
+ON I.[FormName]= C.[FormName]
+WHERE C.[StatusID] NOT IN (1,2)
+OPTION (MAXDOP 1)
+END
+
+
+
+WAITFOR DELAY '00:00:00.05' -- Wait for 5 ms
+
+ -- Set Message for Inactivated logs
+
+BEGIN
+UPDATE [EC].[Inactivations_Stage]
+SET [Message] = 'Successful'
+,[ProcessDate] = GETDATE()
+FROM [EC].[Inactivations_Stage] I JOIN [EC].[Coaching_Log] C 
+ON I.[FormName]= C.[FormName]
+WHERE C.[StatusID] = 2
+AND [Message] IS NULL
+OPTION (MAXDOP 1)
+END
+
+
+WAITFOR DELAY '00:00:00.05' -- Wait for 5 ms
+
+ IF @strLogType = 'Warning'
+ 
+  -- Warning logs 
+ -- Set Message for Invalid Form Names
+
+BEGIN
+UPDATE [EC].[Inactivations_Stage]
+SET [Message] = 'Form Name does not Exist.'
+,[ProcessDate] = GETDATE()
+WHERE [FormName] NOT IN 
+(SELECT DISTINCT FormName FROM [EC].[Warning_Log] C WITH (NOLOCK))
+OPTION (MAXDOP 1)
+END
+
+WAITFOR DELAY '00:00:00.05' -- Wait for 5 ms
+
+ -- Set Message for Invalid Status
+
+BEGIN
+UPDATE [EC].[Inactivations_Stage]
+SET [Message] = 'Invalid Status for Inactivation.'
+,[ProcessDate] = GETDATE()
+FROM [EC].[Inactivations_Stage] I JOIN [EC].[Warning_Log] W 
+ON I.[FormName]= W.[FormName]
+WHERE W.[StatusID] <> 1
+OPTION (MAXDOP 1)
+END
+
+
+WAITFOR DELAY '00:00:00.05' -- Wait for 5 ms
+
+
+-- Inactivate Coaching logs 
+
+BEGIN
+UPDATE [EC].[Warning_Log]
+SET [StatusID] = 2
+FROM [EC].[Inactivations_Stage] I JOIN [EC].[Warning_Log]W
+ON I.[FormName]= W.[FormName]
+WHERE W.[StatusID] <> 2
+OPTION (MAXDOP 1)
+END
+
+
+WAITFOR DELAY '00:00:00.05' -- Wait for 5 ms
+
+ -- Set Message for Inactivated logs
+
+BEGIN
+UPDATE [EC].[Inactivations_Stage]
+SET [Message] = 'Successful'
+,[ProcessDate] = GETDATE()
+FROM [EC].[Inactivations_Stage] I JOIN [EC].[Warning_Log] W
+ON I.[FormName]= W.[FormName]
+WHERE W.[StatusID] = 2
+AND [Message] IS NULL
+OPTION (MAXDOP 1)
+END
+
+
+WAITFOR DELAY '00:00:00.05' -- Wait for 5 ms
+END  -- [EC].[sp_Inactivations_From_Feed]
+
+
+
+
+GO
+
+
+
+
+
+
+
+
+--***************************************************************************************************************
