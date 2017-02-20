1d70987ba57c701465eb9b8667d4cbc13e675f04
susmitha.palacherla <susmitha.palacherla@ad.tfs.local>
2017-02-20 22:19:54 +0000

    TFS 5649. Updates from V&V feedback.
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C36623

--- Code/DB/Functions/fn_strNPNDescriptionFromCode.sql
+++ Code/DB/Functions/fn_strNPNDescriptionFromCode.sql
@@ -1,9 +1,9 @@
 /*
-fn_strNPNDescriptionFromCode(01).sql
+fn_strNPNDescriptionFromCode(02).sql
 Last Modified Date: 2/20/2017
 Last Modified By: Susmitha Palacherla
 
-
+Version 02: Updted from V&V feedback - TFS 5649 - 02/20/2017
 
 Version 01: Document Initial Revision - TFS 5649 - 02/17/2017
 
@@ -18,12 +18,12 @@ IF EXISTS (
 )
    DROP FUNCTION [EC].[fn_strNPNDescriptionFromCode]
 GO
-
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 
 
@@ -43,17 +43,23 @@ AS
 BEGIN
   DECLARE @strDescription NVARCHAR(MAX)
   
-  IF @strCode IS NOT NULL
+IF @strCode IS NOT NULL
   BEGIN
   SET @strDescription = (SELECT [NPNDescription] FROM [EC].[NPN_Description]
-                         WHERE [NPNCode]= @strCode)       
-	END
+                         WHERE [NPNCode]= @strCode)     
+                         
+  IF     @strDescription  IS NULL 
+  SET    @strDescription = @strCode                        
+END
     ELSE
     SET @strDescription = NULL
+    
+
         
 RETURN @strDescription
 
 END  -- fn_strNPNDescriptionFromCode
+
 
 
 
