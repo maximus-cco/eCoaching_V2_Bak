21b1fb5f39e3dc0a40d8f4220c4f196c888d4af2
lili.huang <lili.huang@ad.tfs.local>
2017-03-17 16:15:54 +0000

    TFS 5914 - Add 'Delete Log' function. Added EmployeeLogBaseController.cs
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C36831

---
+++ Code/Web Development/eCLAdmin/eCLAdmin/Controllers/EmployeeLogBaseController.cs
@@ -0,0 +1,33 @@
+﻿using eCLAdmin.Services;
+using log4net;
+using System.Web.Mvc;
+
+namespace eCLAdmin.Controllers
+{
+    public class EmployeeLogBaseController : BaseController
+    {
+        private readonly ILog logger = LogManager.GetLogger(typeof(EmployeeLogBaseController));
+
+        protected readonly IEmployeeLogService employeeLogService;
+
+        public EmployeeLogBaseController(IEmployeeLogService employeeLogService)
+        {
+            logger.Debug("Entered EmployeeLogBaseController(IEmployeeLogService)");
+            this.employeeLogService = employeeLogService;
+        }
+
+        protected ActionResult GetLogDetail(int logId, bool isCoaching)
+        {
+            logger.Debug("Entered GetLogDetail");
+
+            string partialView = "_CoachingDetail";
+
+            if (!isCoaching)
+            {
+                partialView = "_WarningDetail";
+            }
+
+            return PartialView(partialView, employeeLogService.GetLogDetail(logId, isCoaching));
+        }
+    }
+}
