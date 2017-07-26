d6fc6ddab90d42bddde8bda1b1bba9cc9c5de803
susmitha.palacherla <susmitha.palacherla@ad.tfs.local>
2017-07-26 21:31:55 +0000

    Updated for TFS 7174. Incorporate HNC and ICC feeds.
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C38286

--- Code/DB/Stored Procedures/sp_InsertInto_ETS_Rejected.sql
+++ Code/DB/Stored Procedures/sp_InsertInto_ETS_Rejected.sql
@@ -1,9 +1,9 @@
 /*
-sp_InsertInto_ETS_Rejected(01).sql
-Last Modified Date: 1/18/2017
+sp_InsertInto_ETS_Rejected(02).sql
+Last Modified Date: 7/26/2017
 Last Modified By: Susmitha Palacherla
 
-
+Version 02: Updated to incorporate HNC and ICC Reports per TFS 7174 - 07/26/2017
 
 Version 01: Document Initial Revision - TFS 5223 - 1/18/2017
 
@@ -30,6 +30,7 @@ GO
 
 
 
+
 -- =============================================
 -- Author:		   Susmitha Palacherla
 -- Create date: 11/19/14
@@ -38,6 +39,7 @@ GO
 -- Last Modified Date - 01/05/2015
 -- Last Modified By - Susmitha Palacherla
 -- Modified per scr 14031 to incorporate the compliance reports.
+-- Modified to support HNC and ICC Reports for CSRs. TFS 7174 - 07/21/2017
 -- =============================================
 CREATE PROCEDURE [EC].[sp_InsertInto_ETS_Rejected] 
 
@@ -45,7 +47,7 @@ AS
 BEGIN
 
 -- Determine and populate Reject Reasons
-
+-- Reject logs with incorrect or non-supported ETS report Code 
 
 BEGIN
 UPDATE [EC].[ETS_Coaching_Stage]
@@ -55,11 +57,11 @@ WHERE LEFT([Report_Code],LEN([Report_Code])-8) NOT IN
 	
 OPTION (MAXDOP 1)
 END  
-    
-  
-    
-WAITFOR DELAY '00:00:00.03' -- Wait for 5 ms
 
+    
+WAITFOR DELAY '00:00:00.03' -- Wait for 3 ms
+
+-- Reject Logs with Employee not found.
 
 BEGIN
 UPDATE [EC].[ETS_Coaching_Stage]
@@ -70,39 +72,47 @@ AND [Reject_Reason]is NULL
 	
 OPTION (MAXDOP 1)
 END  
-    
-    
-WAITFOR DELAY '00:00:00.03' -- Wait for 5 ms
+       
+WAITFOR DELAY '00:00:00.03' -- Wait for 3 ms
 
-
+-- Reject Logs that can only be for Sup Module and Employee does not have a Sup job code.
 BEGIN
 UPDATE [EC].[ETS_Coaching_Stage]
-SET [Reject_Reason]= CASE WHEN LEFT(Report_Code,LEN(Report_Code)-8) IN ('EA', 'EOT','FWH','HOL','ITD', 'ITI', 'UTL', 'OAE')
-AND [Emp_Role]not in ( 'C','S') THEN N'Employee does not have a CSR or Supervisor job code.'
-ELSE NULL END
-WHERE [Emp_Role] NOT in ('C','S')AND [Reject_Reason]is NULL
-OPTION (MAXDOP 1)
-END  
-   
-
-    
-WAITFOR DELAY '00:00:00.03' -- Wait for 5 ms
-
-
-BEGIN
-UPDATE [EC].[ETS_Coaching_Stage]
-SET [Reject_Reason]= CASE WHEN LEFT(Report_Code,LEN(Report_Code)-8) IN ('FWHA','HOLA','ITDA', 'ITIA', 'UTLA','OAS') 
+SET [Reject_Reason]= CASE WHEN LEFT(Report_Code,LEN(Report_Code)-8) IN ('FWHA','HOLA','ITDA', 'ITIA', 'UTLA','OAS','EOT') 
 AND [Emp_Role] <> 'S' THEN N'Approver does not have a Supervisor job code.'
 ELSE NULL END
 WHERE [Emp_Role] <> 'S' and [Reject_Reason]is NULL
 OPTION (MAXDOP 1)
 END  
+       
+WAITFOR DELAY '00:00:00.03' -- Wait for 3 ms
+
+-- Reject Logs that can only be for CSR Module and Employee does not have a CSR job code.
+BEGIN
+UPDATE [EC].[ETS_Coaching_Stage]
+SET [Reject_Reason]= CASE WHEN LEFT(Report_Code,LEN(Report_Code)-8) IN ('HNC', 'ICC')
+AND [Emp_Role] <> 'C' THEN N'Employee does not have a CSR job code.'
+ELSE NULL END
+WHERE [Emp_Role] <> 'C' AND [Reject_Reason]is NULL
+OPTION (MAXDOP 1)
+END  
+       
+WAITFOR DELAY '00:00:00.03' -- Wait for 3 ms
+
+--Reject logs that can be for either CSR or Sup Module and do not have supporting job codes.
+BEGIN
+UPDATE [EC].[ETS_Coaching_Stage]
+SET [Reject_Reason]= CASE WHEN LEFT(Report_Code,LEN(Report_Code)-8) IN ('EA','FWH','HOL','ITD', 'ITI', 'UTL', 'OAE')
+AND [Emp_Role] not in ( 'C','S') THEN N'Employee does not have a CSR or Supervisor job code.'
+ELSE NULL END
+WHERE [Emp_Role] NOT in ('C','S')AND [Reject_Reason]is NULL
+OPTION (MAXDOP 1)
+END  
    
+   
+WAITFOR DELAY '00:00:00.03' -- Wait for 3 ms
 
-
-    
-WAITFOR DELAY '00:00:00.03' -- Wait for 5 ms
-    
+ 
 
 -- Write rejected records to Rejected table.
 
@@ -168,9 +178,6 @@ OPTION (MAXDOP 1)
 END
 
 END  -- [EC].[sp_InsertInto_ETS_Rejected]
-
-
-
-
 GO
+
 
