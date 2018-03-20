59cf3f6dd33aae46e49166fcebac5ce9daf3f6df
lili.huang <lili.huang@ad.tfs.local>
2018-03-20 15:57:36 +0000

    10493 - Alpha character employee id causing error in survey; Updated EmployeeID to be String
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C39722

--- Code/Web Development/eCoaching/Models/Survey/Survey.vb
+++ Code/Web Development/eCoaching/Models/Survey/Survey.vb
@@ -1,8 +1,8 @@
 ﻿<Serializable()> Public Class Survey
     Public Property ID As Integer
     Public Property LogName As String
-    Public Property EmployeeID As Integer
-    Public Property Questions As ICollection(Of Question)
+	Public Property EmployeeID As String
+	Public Property Questions As ICollection(Of Question)
     Public Property Comment As String
 	Public Property HasHotTopic As Boolean
 	Public Property HasPilot As Boolean
@@ -11,8 +11,8 @@
 	Sub New()
         ID = -1
         LogName = String.Empty
-        EmployeeID = -1
-        Questions = New List(Of Question)
+		EmployeeID = String.Empty
+		Questions = New List(Of Question)
         Comment = String.Empty
 		HasHotTopic = False
 		Status = String.Empty
