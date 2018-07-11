31140e469020d4b106f24cff7d6c3892e9e50936
lili.huang <lili.huang@ad.tfs.local>
2018-07-11 11:40:07 +0000

    TFS 11387 - My Dashboard; Updated app URLs.
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C40480

--- Code/Web Development/eCLAdmin/eCLAdmin/Constants.cs
+++ Code/Web Development/eCLAdmin/eCLAdmin/Constants.cs
@@ -41,8 +41,8 @@ namespace eCLAdmin
 
         public const string WEB_SERVER_NAME_PROD = "f3420-mwbp11.vangent.local";
         public const string WEB_SERVER_NAME_ST = "f3420-mpmd01.vangent.local";
-        public const string ECOACHING_URL_PROD = "https://f3420-mwbp11.vangent.local/coach/default.aspx";
-        public const string ECOACHING_URL_ST = "https://f3420-mpmd01.vangent.local/coach3/default.aspx";
+        public const string ECOACHING_URL_PROD = "https://f3420-mwbp11.vangent.local/eCoachingLog";
+        public const string ECOACHING_URL_ST = "https://f3420-mpmd01.vangent.local/eCoachingLog_st";
 
         public const string SITE_ADMIN_EMAIL = "CCO Quality<CCOQuality@gdit.com>";
         public const string SITE_EMAIL_SUBJECT = "Dashboard and Manage Employee Logs";
