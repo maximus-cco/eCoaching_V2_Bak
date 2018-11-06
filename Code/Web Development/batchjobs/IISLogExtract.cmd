6bf983fdb792a356760a35e61f339ce4dcf57c25
lili.huang <lili.huang@ad.tfs.local>
2018-11-06 16:04:45 +0000

    TFS 12567 - Add IIS activity Reporting to eCoaching Admin Portal
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C41155

---
+++ Code/Web Development/batchjobs/IISLogExtract.cmd
@@ -0,0 +1,40 @@
+@ECHO OFF
+
+FOR /F "TOKENS=1-4 DELIMS=/ " %%A IN ('DATE/T') DO SET DATE=%%D-%%B-%%C
+FOR /F "TOKENS=1-4 DELIMS=:. " %%A IN ('echo %TIME%') DO SET TIME=%%A:%%B:%%C
+ECHO Start: %DATE% %TIME%
+
+REM SET IIS_LOG_DIR=c:\inetpub\logs\LogFiles\W3SVC2\
+SET IIS_LOG_DIR=D:\Apps\logs\
+SET CURRENT_DIR=%~dp0out\
+SET WEB_ROOT=/eCoachingLog_st/
+
+REM Get previous day iis log file name
+FOR /F %%A IN ('cscript //nologo yesterdayDate.vbs') DO SET yesterday=%%A
+SET iisFileName=u_in%yesterday:~2,6%.log
+SET iisFileNameFullPath=%IIS_LOG_DIR%%iisFileName%
+ECHO IIS file: %iisFileNameFullPath%
+
+REM Set output csv file name
+SET csvFileName=ecl_iis_%yesterday:~2,6%.csv
+SET csvFileNameFullPath=%CURRENT_DIR%%csvFileName%
+ECHO CSV file: %csvFileNameFullPath%
+
+REM Extract ecl user web activities to cvs file
+"C:\Program Files (x86)\Log Parser 2.2\logparser" ^
+"SELECT EXTRACT_SUFFIX(UserName, 0, '\\') AS UserName, TO_TIMESTAMP(Date, Time) AS DateTime, EXTRACT_SUFFIX(Target, 0, '%WEB_ROOT%') AS Target, EXTRACT_TOKEN(Target, 2, '/') AS PageName,StatusCode INTO %csvFileNameFullPath% ^
+ FROM %iisFileNameFullPath% ^
+ WHERE Target LIKE '%WEB_ROOT%%%' ^
+   AND UserName IS NOT NULL ^
+   AND Target NOT LIKE '%WEB_ROOT%%%KeepSessionAlive%%' ^
+   AND Target NOT LIKE '%WEB_ROOT%%%Scripts/%%' ^
+   AND Target NOT LIKE '%WEB_ROOT%Content/%%' ^
+   AND Target NOT LIKE '%WEB_ROOT%fonts/%%' ^
+   AND Target NOT LIKE '%WEB_ROOT%bundles/%%' ^
+   AND Target NOT LIKE '%WEB_ROOT%Error'" -i:IIS -o:CSV 
+
+XCOPY /D /Y %csvFileNameFullPath% \\f3420-ecldbd01\Data\Coaching\IISLogImport\Current
+
+FOR /F "TOKENS=1-4 DELIMS=/ " %%A IN ('DATE/T') DO SET DATE=%%D-%%B-%%C
+FOR /F "TOKENS=1-4 DELIMS=:. " %%A IN ('echo %TIME%') DO SET TIME=%%A:%%B:%%C
+ECHO Finish: %DATE% %TIME%
---
+++ Code/Web Development/batchjobs/yesterdayDate.vbs
@@ -0,0 +1,2 @@
+d = date() - 1
+wscript.echo year(d) * 10000 + month(d) * 100 + day(d)
