f7cdf19c9b7ff014185541f1b2e59ee7dd260221
lili.huang <lili.huang@ad.tfs.local>
2018-11-27 19:49:43 +0000

    TFS 12567 - Add IIS activity Reporting function to eCoaching Admin Portal;
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C41276

---
+++ Code/DB/Stored Procedures/sp_InsertInto_IISLog_From_Stage.sql
@@ -0,0 +1,48 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+
+IF NOT EXISTS (
+  SELECT * FROM INFORMATION_SCHEMA.ROUTINES 
+  WHERE SPECIFIC_SCHEMA = N'ec' AND SPECIFIC_NAME = N'sp_InsertInto_IISLog_From_Stage' 
+)
+BEGIN
+	EXEC sp_executeSQL N'CREATE PROCEDURE ec.sp_InsertInto_IISLog_From_Stage AS SELECT ''Procedure Stub'';';
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [EC].[sp_InsertInto_IISLog_From_Stage]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	OPEN SYMMETRIC KEY [CoachingKey]  
+	DECRYPTION BY CERTIFICATE [CoachingCert] 
+
+	INSERT INTO EC.IISLog (EmployeeID, IISLogDateTime, [Target], PageName, StatusCode)
+	SELECT [EC].[fn_nvcGetEmpIdFromLanId](username, getdate()), IISLogDateTime, [Target], PageName, StatusCode
+	FROM EC.IISLog_Stage
+
+	CLOSE SYMMETRIC KEY [CoachingKey] 
+END
+GO
