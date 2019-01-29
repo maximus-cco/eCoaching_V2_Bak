960e7995f3096214e373d12efda79893180fd499
lili.huang <lili.huang@ad.tfs.local>
2019-01-29 16:30:22 +0000

    TFS 13282 - IQS job (Quality Load) causing timeoutsj; Minor update
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C41669

--- Code/DB/Stored Procedures/sp_InsertInto_Coaching_Log_NPN.sql
+++ Code/DB/Stored Procedures/sp_InsertInto_Coaching_Log_NPN.sql
@@ -94,7 +94,7 @@ BEGIN
 	  ,0                  -- isUCID
 	  ,1                  -- isVerintID
 	  ,qcs.Journal_ID     -- VerintID
-	  ,REPLACE(EC.fn_nvcHtmlEncode([EC].[fn_strNPNDescriptionFromCode](qs.[Summary_CallerIssues])), CHAR(13) + CHAR(10), '<br />') + qs.Journal_ID [Description] -- Description
+	  ,REPLACE(EC.fn_nvcHtmlEncode([EC].[fn_strNPNDescriptionFromCode](qcs.[Summary_CallerIssues])), CHAR(13) + CHAR(10), '<br />') + qcs.Journal_ID [Description] -- Description
 	  ,GetDate()          -- SubmittedDate
 	  ,qcs.Call_Date      -- StartDate
 	  ,0                  -- isCSE
