7f54a02baa7554a52a578c40a25923521ed5c0a9
Susmitha C. Palacherla <OMNICLOUD\scpalacherla>
2019-12-13 20:37:42 +0000

    TFS 16261 - WPOP as Analyst
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C44229

--- Code/DB/Functions/fn_strGetUserRole.sql
+++ Code/DB/Functions/fn_strGetUserRole.sql
@@ -1,8 +1,9 @@
 /*
-fn_strGetUserRole(04).sql
-Last Modified Date: 10/29/2019
+fn_strGetUserRole(05).sql
+Last Modified Date: 12/12/2019
 Last Modified By: Susmitha Palacherla
 
+Revision 05: Added WPOP12 to Analyst Role - TFS 16261 - 12/12/2019
 Revision 04: Added WPOP12 to ARC Role - TFS 15859 - 10/28/2019
 Revision 03: Added logic for Manager role for WPPM job codes - TFS 12467 - 10/29/2018
 Revision 02: Added logic for Analyst Role . TFS 12316 - 10/11/2018
@@ -25,7 +26,6 @@ GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
 -- =============================================
 -- Author:		Susmitha Palacherla
 -- Create date:  5/16/2018
@@ -34,8 +34,9 @@ GO
 -- Revision History:
 -- Initial Revision. Created during Mydashboard move to new architecture - TFS 7137 - 05/16/2018 
 -- Added logic for Analyst Role . TFS 12316 - 10/11/2018
---Added logic for Manager role for WPPM job codes - TFS 12467 - 10/29/2018
+-- Added logic for Manager role for WPPM job codes - TFS 12467 - 10/29/2018
 -- Added WPOP12 to ARC Role. TFS 15859 - 10/28/2019
+-- Moved WPOP12 to Analyst Role. TFS 16261 - 12/11/2019 
 -- =============================================
 
 CREATE FUNCTION [EC].[fn_strGetUserRole] 
@@ -50,12 +51,12 @@ BEGIN
 	@strUserRole nvarchar(20)
 	
 SET @strEmpJobCode = (SELECT Emp_Job_Code From EC.Employee_Hierarchy
-WHERE Emp_ID = @strEmpID)
+WHERE Emp_ID = @strEmpID);
 
  SET @strUserRole = 
  (SELECT CASE 
  WHEN (@strEmpJobCode LIKE 'WACS0%' AND [EC].[fn_strCheckIf_ACLRole](@strEmpID, 'ARC') = 0) THEN 'CSR'
- WHEN ((@strEmpJobCode LIKE 'WACS0%' OR @strEmpJobCode = 'WPOP12') AND [EC].[fn_strCheckIf_ACLRole](@strEmpID, 'ARC') = 1) THEN 'ARC'
+ WHEN (@strEmpJobCode LIKE 'WACS0%' AND [EC].[fn_strCheckIf_ACLRole](@strEmpID, 'ARC') = 1) THEN 'ARC'
  WHEN (@strEmpJobCode LIKE 'WACQ0%' OR @strEmpJobCode LIKE 'WACQ12' OR @strEmpJobCode LIKE 'WIHD0%'
  OR  @strEmpJobCode LIKE 'WTTR1%' OR  @strEmpJobCode LIKE 'WTID%'  ) THEN 'Employee'
  WHEN @strEmpJobCode LIKE 'WH%' THEN 'HR'
@@ -67,14 +68,13 @@ WHERE Emp_ID = @strEmpID)
  THEN 'Manager'
  WHEN  [EC].[fn_strCheckIf_ACLRole](@strEmpID, 'SRM') = 1 THEN 'SrManager'
  WHEN  [EC].[fn_strCheckIf_ACLRole](@strEmpID, 'DIR') = 1 THEN 'Director'
- --WHEN  (@strEmpJobCode LIKE 'WPPM%' AND [EC].[fn_strCheckIf_ACLRole](@strEmpID, 'ECL') = 1 ) THEN 'Analyst'
-  --WHEN @strEmpJobCode LIKE 'WH%' THEN 'HR'
- ELSE 'Restricted' END)
+ WHEN  @strEmpJobCode LIKE 'WPOP12' THEN 'Analyst'
+ ELSE 'Restricted' END);
 
  RETURN   @strUserRole
   
 END --fn_strGetUserRole
-GO
 
+GO
 
 
