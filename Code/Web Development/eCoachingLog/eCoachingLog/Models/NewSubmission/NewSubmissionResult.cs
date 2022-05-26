1df75d8d9d84cd442090f07255d882a1401845ed
Lili Huang <OMNICLOUD\lhuang>
2022-05-26 20:03:10 +0000

    24732 - Team coaching log submission; Missing file
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C51524

---
+++ Code/Web Development/eCoachingLog/eCoachingLog/Models/NewSubmission/NewSubmissionResult.cs
@@ -0,0 +1,11 @@
+﻿using eCoachingLog.Models.Common;
+
+namespace eCoachingLog.Models
+{
+    public class NewSubmissionResult
+    {
+        public Employee Employee { get; set; }
+        public string LogName { get; set; }
+        public string Error { get; set; }
+    }
+}
