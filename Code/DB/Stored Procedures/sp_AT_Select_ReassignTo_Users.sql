b1f07b1b3ab590f0bccbeb7d402155de2ef283ce
Susmitha C. Palacherla <OMNICLOUD\scpalacherla>
2022-09-16 15:08:51 +0000

    TFS 25229 - Add ability to search by formname in Admin Tool. Additional changes.
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C51945

--- Code/DB/Stored Procedures/sp_AT_Select_ReassignTo_Users.sql
+++ Code/DB/Stored Procedures/sp_AT_Select_ReassignTo_Users.sql
@@ -1,19 +1,8 @@
-
-IF EXISTS (
-  SELECT * 
-    FROM INFORMATION_SCHEMA.ROUTINES 
-   WHERE SPECIFIC_SCHEMA = N'EC'
-     AND SPECIFIC_NAME = N'sp_AT_Select_ReassignTo_Users' 
-)
-   DROP PROCEDURE [EC].[sp_AT_Select_ReassignTo_Users]
-GO
-
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
 
 --	====================================================================
 --	Author:			Susmitha Palacherla
@@ -28,6 +17,7 @@ GO
 -- Updated to incorporate a follow-up process for eCoaching submissions - TFS 13644 -  08/28/2019
 -- Modified during changes to QN Workflow. TFS 22187 - 09/20/2021
 -- Modified to support cross site access for Virtual East Managers. TFS 23378 - 10/29/2021 
+-- Modified to add ability to search by FormName . TFS 25229 - 08/29/2022
 --	=====================================================================
 CREATE OR ALTER PROCEDURE [EC].[sp_AT_Select_ReassignTo_Users] 
 @strRequesterin nvarchar(30),@strFromUserIdin nvarchar(10), @intModuleIdin INT, @intStatusIdin INT
@@ -39,7 +29,9 @@ DECLARE
 @nvcRequesterID nvarchar(10),
 @intRequesterSiteID int,
 @intFromUserSiteID int,
-@strSelect nvarchar(1000),
+@strConditionalSelect nvarchar(100),
+@strConditionalRestrict nvarchar(100),
+@strConditionalStatus nvarchar(100),
 @dtmDate datetime,
 @NewLineChar nvarchar(2)
 
@@ -53,45 +45,56 @@ SET @nvcRequesterID = EC.fn_nvcGetEmpIdFromLanID(@strRequesterin,@dtmDate)
 SET @intFromUserSiteID = EC.fn_intSiteIDFromEmpID(@strFromUserIdin)
 SET @NewLineChar = CHAR(13) + CHAR(10)
 
-IF ((@intStatusIdin IN (6,8,10, 11,12) AND @intModuleIdin IN (1,3,4,5))
-OR (@intStatusIdin = 5 AND @intModuleIdin = 2))
+-- Determine whether to fetch Supervisor or Manager depending on Status and Module combination
 
+IF ((@intStatusIdin IN (6,8,10,11,12,-2) AND @intModuleIdin NOT IN (-1,2))
+OR (@intStatusIdin IN (-2,5) AND @intModuleIdin = 2))
 
 BEGIN
-SET @nvcSQL = N'SELECT DISTINCT sh.EMP_ID UserID, vsh.Emp_Name UserName
-FROM [EC].[Employee_Hierarchy]eh JOIN [EC].[Coaching_Log] cl WITH(NOLOCK) ON
-cl.EmpID = eh.Emp_ID JOIN [EC].[Employee_Hierarchy]sh
-ON eh.SUP_ID = sh.EMP_ID JOIN [EC].[View_Employee_Hierarchy] vsh
-ON sh.Emp_ID = vsh.Emp_ID 
-WHERE (cl.SiteID = '''+CONVERT(NVARCHAR,@intFromUserSiteID)+''' OR eh.Mgr_ID = '''+@nvcRequesterID+''' )
-AND (vsh.Emp_Name is NOT NULL AND vsh.Emp_Name <> ''Unknown'')
-AND eh.SUP_ID <> '''+@strFromUserIdin+''' 
-AND eh.Active NOT IN (''T'',''D'')
-AND sh.Active = ''A''
-Order By UserName'
+SET @strConditionalSelect = N'SELECT DISTINCT eh.SUP_ID UserID, veh.SUP_Name UserName '
+SET @strConditionalRestrict = N'AND eh.SUP_ID <> '''+@nvcRequesterID+''' AND eh.SUP_ID <> '''+@strFromUserIdin+''' ' 
 END
 
 ELSE IF 
-((@intStatusIdin = 5 AND @intModuleIdin IN (1,3,4,5))
+((@intStatusIdin = 5 AND @intModuleIdin NOT in (-1,2))
 OR (@intStatusIdin = 7 AND @intModuleIdin = 2))
 
 BEGIN
-
-SET @nvcSQL = N'SELECT DISTINCT mh.EMP_ID UserID, vmh.Emp_Name UserName
-FROM [EC].[Employee_Hierarchy] eh JOIN [EC].[Coaching_Log] cl WITH(NOLOCK) ON
-cl.EmpID = eh.Emp_ID JOIN [EC].[Employee_Hierarchy]mh
-ON eh.MGR_ID = mh.EMP_ID JOIN [EC].[View_Employee_Hierarchy] vmh
-ON mh.Emp_ID = vmh.Emp_ID 
-WHERE (cl.SiteID = '''+CONVERT(NVARCHAR,@intFromUserSiteID)+''' OR eh.Mgr_ID = '''+@nvcRequesterID+''' )
-AND (vmh.Emp_Name is NOT NULL AND vmh.Emp_Name <> ''Unknown'')
-AND eh.MGR_ID <> '''+@strFromUserIdin+'''
-AND eh.Active NOT IN (''T'',''D'')
-AND mh.Active = ''A''
-Order By UserName'
+SET @strConditionalSelect = N'SELECT DISTINCT eh.MGR_ID UserID, veh.MGR_Name UserName '
+SET @strConditionalRestrict = N' AND eh.MGR_ID <> '''+@nvcRequesterID+'''AND eh.MGR_ID <> '''+@strFromUserIdin+''''
 END
-			 
 
-PRINT @nvcSQL		
+-- Check for a specific Status or All possible Statuses dpending on Status param passed. 
+-- StatusID param value of -2 indicates All
+
+SET @strConditionalStatus = '';
+IF @intStatusIdin = -2
+
+BEGIN
+SET @strConditionalStatus = N'AND cl.StatusId IN (5,6,7,8,10,11,12) '
+END
+
+ELSE
+
+BEGIN
+SET @strConditionalStatus = N'AND cl.StatusId = '''+CONVERT(NVARCHAR,@intStatusIdin)+''' '
+END
+
+
+SET @nvcSQL = @strConditionalSelect +
+'FROM [EC].[View_Employee_Hierarchy] veh JOIN [EC].[Employee_Hierarchy] eh
+ON veh.Emp_ID = eh.Emp_ID JOIN [EC].[Coaching_Log] cl WITH(NOLOCK) 
+ON cl.EmpID = eh.Emp_ID 
+WHERE cl.ModuleID = '''+CONVERT(NVARCHAR,@intModuleIdin)+''' ' +  @NewLineChar 
++ @strConditionalStatus +  @NewLineChar 
++ ' AND (cl.SiteID = '''+CONVERT(NVARCHAR,@intFromUserSiteID)+''' OR eh.Mgr_ID = '''+@nvcRequesterID+''' ) ' +  @NewLineChar 
++ @strConditionalRestrict  +  @NewLineChar 
++ ' AND (veh.Sup_Name is NOT NULL AND veh.Mgr_Name is NOT NULL)
+AND eh.Active NOT IN (''T'',''D'')
+Order By UserName'
+		 
+
+--PRINT @nvcSQL		
 EXEC (@nvcSQL)
 
 CLOSE SYMMETRIC KEY [CoachingKey]  
