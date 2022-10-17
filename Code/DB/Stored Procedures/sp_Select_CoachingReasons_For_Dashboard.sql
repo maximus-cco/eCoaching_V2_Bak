202b6ee2cf114d0a18d5f55b17785985f8f7b725
Susmitha C. Palacherla <OMNICLOUD\scpalacherla>
2022-10-17 16:58:51 +0000

    TFS 25387 Update Search option in the Historical Dashboard. Additional update.
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C52112

--- Code/DB/Stored Procedures/sp_Select_CoachingReasons_For_Dashboard.sql
+++ Code/DB/Stored Procedures/sp_Select_CoachingReasons_For_Dashboard.sql
@@ -1,15 +1,17 @@
+
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+
 --	====================================================================
 --	Author:			Susmitha Palacherla
 --	Create Date:	09/28/2022
 --	Description: *	This procedure selects Coaching Reasons to be displayed in the dashboard
 --  Coaching Reasons dropdown list.
---  Initial Revision. TFS 25387 - 09/28/2022
+--  Initial Revision. TFS 25387 - 09/26/2022
 --	====================================================================
 CREATE OR ALTER PROCEDURE [EC].[sp_Select_CoachingReasons_For_Dashboard] 
 @nvcEmpID nvarchar(10), @intSourceIdin int
@@ -35,11 +37,14 @@ END
 
 IF @nvcDisplayWarnings = 'YES' AND @intSourceIdin = 120
 
-SET @nvcSQL = 'SELECT CONVERT(nvarchar,[CoachingReasonId]) CoachingReasonId, 
-[CoachingReason] CoachingReason From [EC].[DIM_Coaching_Reason]
+SET @nvcSQL = 'SELECT X.CoachingReasonId, X.CoachingReason FROM
+(SELECT ''-1'' CoachingReasonId, ''All Reasons'' CoachingReason,  01 Sortorder From [EC].[DIM_Coaching_Reason]
+UNION
+SELECT CONVERT(nvarchar,[CoachingReasonId]) CoachingReasonId, 
+[CoachingReason] CoachingReason,  02 Sortorder From [EC].[DIM_Coaching_Reason]
 WHERE CoachingReasonID IN (28,29,30,60)
-AND  [CoachingReason] <> ''Unknown'' 
-ORDER BY CoachingReason'
+AND  [CoachingReason] <> ''Unknown'' ) x
+ORDER BY X.Sortorder, X.CoachingReason'
 
 ELSE
 
