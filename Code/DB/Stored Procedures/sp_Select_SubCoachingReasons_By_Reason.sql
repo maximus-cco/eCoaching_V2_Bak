6e113cc0926457e11b2aead943518cc037ed85da
Susmitha C. Palacherla <OMNICLOUD\scpalacherla>
2023-12-01 21:33:20 +0000

    TFS 27375 - Work at Home Category and Subcategory updates
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C53336

--- Code/DB/Stored Procedures/sp_Select_SubCoachingReasons_By_Reason.sql
+++ Code/DB/Stored Procedures/sp_Select_SubCoachingReasons_By_Reason.sql
@@ -1,33 +1,9 @@
-/*
-sp_Select_SubCoachingReasons_By_Reason(07).sql
-Last Modified Date: 05/06/2020
-Last Modified By: Susmitha Palacherla
-
-Version 07: Updated to customize sort order for Security and Privacy Coaching Reason. TFS 17066 - 05/06/2020
-Version 06: Updated to support changes to warnings workflow. TFS 15803 - 11/21/2019
-Version 05: Modified to support updated requirements to replace ETS with Deltek - TFS 15144 - 08/21/2019
-Version 04: Submissions move to new architecture. Additional changes from V&V feedback - TFS 7136 - 04/30/2018
-Version 03 : Modified to open Encryption key. TFS 10760 - 04/24/2018
-Version 02: Modified during Submissions move to new architecture - TFS 7136 - 04/10/2018
-Version 01: Document Initial Revision - TFS 5223 - 1/18/2017
-
-*/
-
-
-IF EXISTS (
-  SELECT * 
-    FROM INFORMATION_SCHEMA.ROUTINES 
-   WHERE SPECIFIC_SCHEMA = N'EC'
-     AND SPECIFIC_NAME = N'sp_Select_SubCoachingReasons_By_Reason' 
-)
-   DROP PROCEDURE [EC].[sp_Select_SubCoachingReasons_By_Reason]
-GO
-
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 --	====================================================================
 --	Author:			Susmitha Palacherla
 --	Create Date:	8/01/14
@@ -38,8 +14,9 @@ GO
 -- Modified to support updated requirements to replace ETS with Deltek - TFS 15144 - 08/21/2019
 -- Updated to support changes to warnings workflow. TFS 15803 - 11/21/2019
 -- Updated to customize sort order for Security and Privacy Coaching Reason. TFS 17066 - 05/06/2020
+-- Updated to support changes to Work at Home CRs and Sub CRs. TFS 27375- 11/30/2023
 --	=====================================================================
-CREATE PROCEDURE [EC].[sp_Select_SubCoachingReasons_By_Reason] 
+CREATE OR ALTER PROCEDURE [EC].[sp_Select_SubCoachingReasons_By_Reason] 
 @intReasonIDin INT, @intModuleIDin INT, @strSourcein nvarchar(30), @nvcEmpIDin nvarchar(10)
 
 AS
@@ -60,7 +37,7 @@ and [IsActive] = 1
 AND ' + @strSourcein +' = 1
 Order by CASE WHEN [SubCoachingReason] in
  (''Other: Specify reason under coaching details.'', ''Other Policy (non-Security/Privacy)'', ''Other: Specify'', ''Other Policy Violation (non-Security/Privacy)'',
- ''Other Security & Privacy'') Then 1
+ ''Other Security & Privacy'', ''Other WFH Agreement Violations: Specify reason under coaching details.'') Then 1
  Else 0 END ,
  CASE WHEN ([SubCoachingReason] like ''Disclosure%'') Then 0 Else 1  END ,
  CASE WHEN [SubCoachingReason] in
