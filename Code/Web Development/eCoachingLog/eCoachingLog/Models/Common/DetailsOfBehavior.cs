c51aa818f519abaadfbad0919043727ddc4b91a7
Lili Huang <OMNICLOUD\lhuang>
2024-03-19 19:13:47 +0000

    27927 - QN Olympic Reward logs. Added images
    git-tfs-id: [https://tfs.omnicloud.local/tfs/cco%20program/]$/eCoaching_V2;C53649

--- Code/Web Development/eCoachingLog/eCoachingLog/Models/Common/DetailsOfBehavior.cs
+++ Code/Web Development/eCoachingLog/eCoachingLog/Models/Common/DetailsOfBehavior.cs
@@ -6,25 +6,53 @@
         public string Medal { get; set; }
         public string Heating { get; set; }
 
+        // medals
         public bool IsGold { get; set; }
         public bool IsSilver { get; set; }
         public bool IsBronze { get; set; }
         public bool IsHonorMention { get; set; }
-        public bool IsHeat { get; set; }
+        // badges (heating)
+        public bool IsActiveListening { get; set; }
+        public bool IsBusinessProcess { get; set; }
+        public bool IsCallEfficiency { get; set; }
+        public bool IsInfoAccuracy { get; set; }
+        public bool IsIssueResolution { get; set; }
+        public bool IsPersonailityFlexing { get; set; }
+        public bool IsPrivacyDisclaimers { get; set; }
 
         public DetailsOfBehavior()
         {
 
         }
 
-        public DetailsOfBehavior(string behavior, bool isGold, bool isSilver, bool isBronze, bool isHonorMention, bool isHeat)
+        public DetailsOfBehavior(
+            string behavior, 
+            // medals
+            bool isGold, 
+            bool isSilver, 
+            bool isBronze, 
+            bool isHonorMention, 
+            // badges (heating)
+            bool isActiveListening, 
+            bool isBusinessProcess, 
+            bool isCallEfficiency, 
+            bool isInfoAccuracy, 
+            bool isIssueResolution,
+            bool isPersonailityFlexing, 
+            bool isPrivacyDisclaimers)
         {
-            this.Behavior = behavior;
-            this.IsGold = isGold;
-            this.IsSilver = isSilver;
-            this.IsBronze = isBronze;
-            this.IsHonorMention = isHonorMention;
-            this.IsHeat = isHeat;
+            Behavior = behavior;
+            IsGold = isGold;
+            IsSilver = isSilver;
+            IsBronze = isBronze;
+            IsHonorMention = isHonorMention;
+            IsActiveListening = isActiveListening;
+            IsBusinessProcess = isBusinessProcess;
+            IsCallEfficiency = isCallEfficiency;
+            IsInfoAccuracy = isInfoAccuracy;
+            IsIssueResolution = isIssueResolution;
+            IsPersonailityFlexing = isPersonailityFlexing;
+            IsPrivacyDisclaimers = isPrivacyDisclaimers;
         }
     }
 }
